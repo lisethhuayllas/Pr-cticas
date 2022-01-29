927623527839b857132cd624e7963b361a81aaee
lisethhuayllas <lisethhuayllas2@gmail.com>
2022-01-28 22:16:18 -0500

    Actualizando

--- Vistas/Modulos/ingreso.php
+++ Vistas/Modulos/ingreso.php
@@ -4,14 +4,31 @@ $resultado = $adminC->IngresoC();
 
 ?>
 
-<br> <!-- Vistas/Modulos/ingreso.php -->
-<h1>INICIAR SESIÓN</h1>
+<div class=" d-flex flex-column text-center px-5 mt-3 mb-3"><h4>EasyNotes</h4> </div>
 
-<form method="post" action="">
-	<input type="text" placeholder="Usuario" name="usuarioI" required>
-	<input type="password" placeholder="Contraseña" name="passwordI" required>
-	<input type="submit" value="Ingresar">
-</form>
+<div class="container mt-5 mb-5 d-flex justify-content-center">
+   <div class="card px-4 py-4">
+        <div class=" d-flex flex-column text-center px-5 mt-3 mb-3"><h4>INICIAR SESIÓN</h4> </div>
+
+	      <form action="">
+	        <div class="from group">
+	          <label class="sr-only" for="username"><b>Usuario</b></label>
+	          <input type="username" name="username" id="username" class="form-control">
+
+	          <label class="sr-only" for="password"><b>Contraseña</b></label>
+	          <input type="password" name="password" id="password" class="form-control" >
+	          <br>
+	         <input type="submit" value="Ingresar" class="btn btn-primary form-control">
+
+        	</div>
+    	</form>
+	<br>
+
+	<input type="submit" value="Registrarse" class="btn btn-success form-control">
+	<div class="col-12 forgot "><a href="#">Olvidaste tu contraseña?</a></div>
+
+    </div>
+</div>
 
 <?php if (!$resultado):?>
 	USUARIO O CLAVE INVÁLIDOS
